ac2612bf11a0486da7d1f7a9100daeafbdc5d591
Ed Baker <edwbaker@gmail.com>
2023-09-25 11:29:14 +0000

    API documentation

--- templates/api-home.php
+++ templates/api-home.php
@@ -1,2 +1,21 @@
-<?php
-print t("API Home");
+<div class="feature-container">
+  <div class="feature">
+    <h2><?php print("API Home"); ?></h2>
+
+    <p>
+        The API can be used to retrieve information about ontologies and terms in the 
+        Ontomasticon system. The API returns JSON formatted objects.
+    </p>
+  </div>
+
+  <div class="feature">
+    <h3>Terms endpoints</h3>
+    <p>These endpoints return a term object.</p>
+
+    <h4>/api/term/?term=</h4>
+    <p>Given a term URL returns a JSON term object. The term must be HTML encoded.</p>
+
+    <h4>/api/term/?shortname=</h4>
+    <p>Given a term name returns a JSON term object.</p>
+  </div>
+</div>
