c04733d85c1d5700490a3d195a03555f6b102b5a
Ed Baker <edwbaker@gmail.com>
2023-10-30 09:24:55 +0000

    name api

--- templates/api-term.php
+++ templates/api-term.php
@@ -3,12 +3,20 @@ header('Content-Type: application/json; charset=utf-8');
 global $db;
 $config = getConfig($db);
 if (isset($_GET["term"])) {
-    $parts = explode("/", $_GET["term"]);
-    if ($parts[3] == "cv") {
-      $term = getTerm(explode("#",$parts[4])[1]);
-    }
+  $parts = explode("/", $_GET["term"]);
+  if ($parts[3] == "cv") {
+    $term = getTerm(explode("#",$parts[4])[1]);
+  }
 } else if (isset($_GET["shortname"])) {
-   $term = getTerm($_GET["shortname"]);
+  $term = getTerm($_GET["shortname"]);
+} else if (isset($_GET["name"])) {
+  $name = mysqli_real_escape_string($db, $_GET["name"]);
+  $sql = "SELECT `shortname` FROM vocab.`terms` WHERE `name` = '$name'";
+  $res = $db->query($sql);
+  if ($res->num_rows > 0) {
+    $row = $res->fetch_assoc();
+    $term = getTerm($row["shortname"]);
+  }
 }
 
 $term["url"] = term2URI($term);
