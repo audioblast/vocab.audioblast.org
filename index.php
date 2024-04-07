a897fc0a032649d2aa5d0f389ac6e48bee528798
Ed Baker <edwbaker@gmail.com>
2024-04-07 22:49:11 +0000

    Quick check.

--- index.php
+++ index.php
@@ -10,8 +10,9 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 $options = array("cost"=>4);
-$hashPassword = password_hash("password",PASSWORD_BCRYPT,$options)
-print($hashPassword);exit;
+$hashPassword = password_hash("password",PASSWORD_BCRYPT,$options);
+print($hashPassword);
+exit;
 
 //Check we can connect to the database
 if (file_exists("settings/db.php")) {
